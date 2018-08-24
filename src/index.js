9cadf86bc9fb09370c634cf1dd1fadfb7997c2aa
Spencer James <spencerleejames@gmail.com>
2018-08-23 22:21:16 -0700

    componetized UI, logic + api's working, needs styling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import QuoteMachine from './QuoteMachine';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<QuoteMachine />, document.getElementById('root'));
